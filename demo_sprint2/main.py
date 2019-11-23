9db0be65daff385405d96116113816d2c8af70be
AnaisW92 <anais_walle@yahoo.fr>
2019-11-23 16:15:27 +0100

    add BLE files

---
+++ demo_sprint2/main.py
@@ -0,0 +1,27 @@
+from bleGattServer.bleThreads import BLETransmitterThread,BLEServer
+from threading import Event
+
+	
+def main():
+	runRaspiCodeEvent = Event()
+	runRaspiCodeEvent.set()
+	
+	#BLE part
+	bleServer = BLEServer()
+	bleTransmitterThread= BLETransmitterThread(bleServer,runRaspiCodeEvent) #for transmitting messages to the server
+	
+
+	try:
+		bleTransmitterThread.start()
+		bleServer.run() ############################### !! À METTRE APRÈS AVOIR DÉMARRÉ TOUS LES THREADS
+				
+	except KeyboardInterrupt:
+		print ('Attempting to close all threads')
+        runRaspiCodeEvent.clear()
+        bleTransmitterThread.join()
+        print ('All threads successfully closed')
+        bleServer.quit()
+		
+if __name__ == '__main__':
+    main()
+
