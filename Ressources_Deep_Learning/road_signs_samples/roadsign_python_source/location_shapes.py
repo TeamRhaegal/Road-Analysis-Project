da33f01697ec0abc1be15b9609f80064d8fc09e0
Vincent pinel <vincentguillaume.pinel@gmail.com>
2019-11-23 10:37:35 +0100

    improved location + classification model

--- Ressources_Deep_Learning/road_signs_samples/roadsign_python_source/location_shapes.py
+++ Ressources_Deep_Learning/road_signs_samples/roadsign_python_source/location_shapes.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-#!/usr/bin/env python
-
-# import the necessary packages
-import imutils
-import cv2
-import time
-import numpy as np
-
-class location_shapes(object): 
-    
-    def __init__(self):
-        
-        pass
-    
-    #!/usr/bin/env python
-
 """
     Program used to detect the location of a stop sign for the moment (and others later depending on the desired shape). 
     The location of the road sign in an image is done using image modification techniques, and using the openCV library (not machine learning)
     The location is returned from the "find_roadsign" function and allows the classification to be focused on the panel. 
 """
-
 
 # import the necessary packages
 import imutils
@@ -71,8 +54,8 @@ class locationShapes(object):
         # loop over the contours
         # first, detect areas larger than minimal size and thiner than maximal size
         area = cv2.contourArea(c)
-        print ("area : {}".format(area))
         if (area >= 500  and area <= 1000000):
+            print ("area : {}".format(area))
             # compute the center of the contour, then detect the name of the
             # shape using only the contour
             M = cv2.moments(c)
@@ -147,94 +130,3 @@ class locationShapes(object):
         # return the name of the shape
         return shape
         pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-filename = "images/demo_imgs/location_demo/raspicam_capture.ppm"
-
-
-if __name__=="__main__" : 
-    sd = ShapeDetector()
-    
-    camera = cv2.VideoCapture(0)
-    camera.set(3, 1380)
-    camera.set(4, 800)
-    
-    cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 1280,720)
-    # load the image and resize it to a smaller factor so that
-    # the shapes can be approximated better
-
-    while(True):
-        #image = cv2.imread(filename)
-        
-        # load camera and read image
-        return_value, image = camera.read()
-        
-        # resize and process image 
-        # convert the resized image to grayscale, blur it slightly,
-        # and threshold it
-        resizedImage = imutils.resize(image, width=500)
-        ratio = image.shape[0] / float(resizedImage.shape[0])
-        grayImage = cv2.cvtColor(resizedImage, cv2.COLOR_BGR2GRAY)
-        blurredImage = cv2.GaussianBlur(grayImage, (5, 5), 0)
-        #thresh1 = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
-        thresh1 = cv2.threshold(blurredImage, 240, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        
-        # process canny edge detection on it
-        cannyImage = cv2.Canny(thresh1, 100, 200)
-        
-        #cv2.imshow('image', cannyImage)       
-        #cv2.waitKey(0)
-    
-        # find contours in the thresholded image
-        contours = cv2.findContours(cannyImage.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = imutils.grab_contours(contours)
-
-        # loop over the contours
-        for c in contours:
-            # first, detect areas larger than minimal size and thiner than maximal size
-            area = cv2.contourArea(c)
-            if (area >= 500  and area <= 7000):
-                print ("areas : {}".format(area))
-                # compute the center of the contour, then detect the name of the
-                # shape using only the contour
-                M = cv2.moments(c)
-                
-                if(M["m00"] != 0):
-                    cX = int((M["m10"] / M["m00"]) * ratio)
-                    cY = int((M["m01"] / M["m00"]) * ratio)
-                    shape = sd.detect(c)
-                    
-                    # multiply the contour (x, y)-coordinates by the resize ratio,
-                    # then draw the contours and the name of the shape on the image
-                    if (shape != "unidentified"):
-                        (x,y,w,h) = cv2.boundingRect(c)
-                        cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
-                        """
-                        c = c.astype("float")
-                        c *= ratio
-                        c = c.astype("int")
-                        cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-                        cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        """
-        # show the output image
-        cv2.imshow("image", image)
-        cv2.waitKey(1)
-        
-      #  cv2.destroyAllWindows()
