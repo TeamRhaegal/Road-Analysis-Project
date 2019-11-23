dcbb698b96cfa5ebdbdb700733fb8cdd385d46b2
Nicolas J <teamrhaegalsiec@gmail.com>
2019-11-23 14:55:47 +0100

    uptade sharedRessources + command.py

--- Raspberry/RhaegalRaspi/sharedRessources.py
+++ Raspberry/RhaegalRaspi/sharedRessources.py
@@ -2,12 +2,15 @@ from mutex import mutex
 from threading import Lock
 
 #Global values
-vitesseRoue = 50  
-Turbo ="off"
-Joystick = "none"
-Mode = "assisted"
+wheelSpeed = 0  
+turbo ="off"
+joystick = "none"
+mode = "assisted"
 sign = "none"
 signWidth = 0
+maxDistanceUS = 10   #distance max ultrasons obstacle
+obstacleRear = 0
+obstacleFront = 0
 
 listMessagesToSend = []
 listMessagesReceived = []
@@ -19,11 +22,11 @@ lockMessagesToSend = Lock()
 lockMessagesReceived = Lock()
 lockConnectedDevice = Lock()
 
-ModeLock =Lock()
-JoystickLock = Lock()
-TurboLock = Lock()
+modeLock =Lock()
+joystickLock = Lock()
+turboLock = Lock()
 signLock = Lock()
 signWidthLock = Lock()
-VitesseLock = Lock()
+speedLock = Lock()
 
 
--- Vehicle_Control/command.py
+++ Vehicle_Control/command.py
@@ -9,7 +9,6 @@ import RPi.GPIO as GPIO
 import can
 import time
 import os
-import struct
 from threading import Thread, Lock
 
 
@@ -22,26 +21,26 @@ MS=0x100
 led=22
 
 #variables globales
-Turbo ="off"
-Joystick = "none"
-Mode = "auto"    #auto pour test unitaire
-Deconnexion = "off"
-Panneau = "none"
-NbPixel = 10
-vitesseRoue = 0
-DistanceMaxUS = 10
-ObstacleRear = 0
-ObstacleFront = 0
+turbo ="off"
+joystick = "none"
+mode = "auto"    #auto pour test unitaire
+deconnexion = "off"
+sign = "none"
+signWidth = 10
+wheelSpeed = 0
+maxDistanceUS = 10
+obstacleRear = 0
+obstacleFront = 0
 
 
-ModeLock =Lock()
-JoystickLock = Lock()
-TurboLock = Lock()
-PanneauLock = Lock()
-NbPixelLock = Lock()
-VitesseLock = Lock()
-ObstacleFrontLock= Lock()
-ObstacleRearLock = Lock()
+modeLock =Lock()
+joystickLock = Lock()
+turboLock = Lock()
+signLock = Lock()
+signWidthLock = Lock()
+speedLock = Lock()
+obstacleFrontLock= Lock()
+obstacleRearLock = Lock()
 
 
 
@@ -55,51 +54,54 @@ class MyCommand(Thread):
     def run(self):
        
 		# mettre la condition de detection d'obstacle ultrasons
-        Width_panneau = 0.20  #nb à déterminer
-        Focal = 595
+        realSignWidth = 0.20  #nb à déterminer en cm
+        focal = 595
         CMD_O = 50
         CMD_V = 50
         CMD_V_A=50
         Temps_necessaire=0.01
         while True : 
             time.sleep(0.01)            
-            ModeLock.acquire()
-            TurboLock.acquire()
-            JoystickLock.acquire()
-            if Turbo == "on" : CMD_Turbo = 100
-            if Turbo == "off" : CMD_Turbo = 75
-            if Joystick == "right" :  CMD_O = 100
-            if Joystick == "left" : CMD_O = 0
-            if Joystick == "front" : CMD_V = CMD_Turbo
-            if Joystick == "right" : CMD_O = 100
-            if Joystick == "right&front" : 
+            modeLock.acquire()
+            turboLock.acquire()
+            joystickLock.acquire()
+            if turbo == "on" : CMD_Turbo = 100
+            if turbo == "off" : CMD_Turbo = 75
+            if joystick == "right" :  
+                CMD_O = 100
+                CMD_V= 50
+            if joystick == "left" : 
+                CMD_O = 0
+                CMD_V = 50
+            if joystick == "front" : CMD_V = CMD_Turbo
+            if joystick == "right&front" : 
                 CMD_O = 100
                 CMD_V = CMD_Turbo
-            if Joystick == "left&front" :
+            if joystick == "left&front" :
                 CMD_O = 0
                 CMD_V = CMD_Turbo
     
-            ModeLock.release()
-            TurboLock.release()
-            JoystickLock.release()   
+            modeLock.release()
+            turboLock.release()
+            joystickLock.release()   
             
-            PanneauLock.acquire()
-            if Panneau == "num panneau": CMD_V_A = 50  #panneau lock
+            signLock.acquire()
+            if sign == "num panneau": CMD_V_A = 50  #panneau lock
             else : CMD_V_A = 100
-            PanneauLock.release()
+            signLock.release()
             CMD_V = CMD_V + 0x80
             CMD_O = CMD_O + 0x80
-            CMD_V_A = CMD_V_A + 0x00
-            if Mode == "auto":
+            CMD_V_A = CMD_V_A + 0x80
+            if mode == "auto":
                 
-                NbPixelLock.acquire()
-                VitesseLock.acquire()
-                if NbPixel != 0 or CMD_V_A == 0 :
-                    Distance_panneau = (Width_panneau*Focal)/NbPixel
-                    Temps_necessaire = Distance_panneau / (vitesseRoue+0.00001)   #calcul du temps à  attendre 
+                signWidthLock.acquire()
+                speedLock.acquire()
+                if signWidth and sign == "stop":
+                    toSignDistance = (realSignWidth*focal)/signWidth
+                    Temps_necessaire = toSignDistance / (wheelSpeed+0.00001)   #calcul du temps à  attendre 
                 else :  Temps_necessaire = 0.01
-                NbPixelLock.release()
-                VitesseLock.release()
+                signWidthLock.release()
+                speedLock.release()
                 
 
                 msg = can.Message(arbitration_id=MOT,data=[CMD_V_A, CMD_V_A, 0x00,0,0,0,0,0],extended_id=False)
@@ -107,12 +109,12 @@ class MyCommand(Thread):
                 self.bus.send(msg)
                 #mode autonome
                
-            elif Mode == "assisted" :
+            elif mode == "assisted" :
                 msg = can.Message(arbitration_id=MOT,data=[CMD_V, CMD_V, CMD_O,0,0,0,0,0],extended_id=False)
                 time.sleep(0.01)
                 self.bus.send(msg)
             # gestion des obstacles : Emergency STOP    
-            elif ObstacleRear or ObstacleFront :
+            elif obstacleRear or obstacleFront :
                 msg = can.Message(arbitration_id=MOT,data=[0x00, 0x00, 0x00,0,0,0,0,0],extended_id=False)
                 time.sleep(0.01)
                 self.bus.send(msg)
@@ -126,21 +128,29 @@ class MySensor(Thread):
         self.bus = bus
 
     def run(self):
-        Perimetre_roue = 0.19 *2*3.14  
+        URL = 180
+        URR = 180
+        URC = 180
+        UFL = 180
+        UFR = 180
+        UFC = 180
+        obstacleFront = 0
+        obstacleRear = 0
+        wheelPerimeter = 0.19 *2*3.14  
         while True :
             msg = self.bus.recv()
             time.sleep(0.01)
             if msg.arbitration_id == MS:
                 Batmes = int.from_bytes(msg.data[2:4], byteorder='big')
                 U = (4095 / Batmes) * (3.3 / 0.2)
-                print(Batmes)
+                print(U)
             if msg.arbitration_id == MS:
                 # Vitesse voiture
-                VitesseLock.acquire()
-                vitesseRoue = int.from_bytes(msg.data[4:6], byteorder='big')
-                vitesseRoue = (0.01*vitesseRoue*Perimetre_roue / 60) #metre/s max : 1.21 m/s donc 4,34 km/H
-                VitesseLock.release()
-                message = "Vitesse :" + str(vitesseRoue)+ ";"
+                speedLock.acquire()
+                wheelSpeed = int.from_bytes(msg.data[4:6], byteorder='big')
+                wheelSpeed = (0.01*wheelSpeed*wheelPerimeter / 60) #metre/s max : 1.21 m/s donc 4,34 km/H
+                speedLock.release()
+                message = "Vitesse :" + str(wheelSpeed)+ ";"
                 print(message)
             if msg.arbitration_id == US2:
                 # ultrason arriere gauche
@@ -176,16 +186,16 @@ class MySensor(Thread):
                 message = "UFC:" + str(distance)+ ";"
                 #print(message)
                 print("---------")
-            '''
-            ObstacleFrontLock.acquire()
-            ObstacleRearLock.acquire()
-            if  URL <DistanceMaxUS or URR<DistanceMaxUS or URC <DistanceMaxUS: ObstacleRear = 1
-            else : ObstacleRear = 0 
-            if UFL<DistanceMaxUS or UFR<DistanceMaxUS or UFC<DistanceMaxUS: ObstacleFront = 1
-            else: ObstacleFront = 0 
-            ObstacleFrontLock.release()
-            ObstacleRearLock.release()
-            '''
+            
+            obstacleFrontLock.acquire()
+            obstacleRearLock.acquire()
+            if  URL <maxDistanceUS or URR<maxDistanceUS or URC <maxDistanceUS: obstacleRear = 1
+            else : obstacleRear = 0 
+            if UFL<maxDistanceUS or UFR<maxDistanceUS or UFC<maxDistanceUS: obstacleFront = 1
+            else: obstacleFront = 0 
+            obstacleFrontLock.release()
+            obstacleRearLock.release()
+            
             
            
                 
