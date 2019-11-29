c96568d38e1dae769ab128ac55204daca2614113
Vincent pinel <vincentguillaume.pinel@gmail.com>
2019-11-29 20:37:34 +0100

    added final dataset V1

--- Housemade_machineLearning_Model/rename_files.py
+++ Housemade_machineLearning_Model/rename_files.py
@@ -3,8 +3,12 @@
 
 import sys, os
 
-path = 'dataset_original/'
+input_path = 'dataset_original/'
+output_path = "dataset/"
+
+
 i = 1
-for filename in os.listdir(path):
-    os.rename(os.path.join(path,filename), os.path.join(path,str(i)+'.jpg'))
+
+for filename in os.listdir(input_path):
+    os.rename(os.path.join(input_path,filename), os.path.join(output_path,str(i)+'.jpg'))
     i = i +1
