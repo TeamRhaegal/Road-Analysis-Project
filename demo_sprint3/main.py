4cf114a0b05ac851ff14fe4f1690de0c28840e63
Nicolas J <teamrhaegalsiec@gmail.com>
2019-12-03 15:27:51 +0100

    updated command.py

---
+++ demo_sprint3/main.py
@@ -0,0 +1,68 @@
+from bleGattServer.bleThreads import BLETransmitterThread,BLEServer
+from threading import Event, Thread, Lock
+import command as C
+import roadsign_detector as detection
+import seprateMessageThread as sm
+import os
+import time
+import can
+	
+def main():
+    print('\n\rCAN Rx test')
+    print('Bring up CAN0....')
+
+    # Bring up can0 interface at 500kbps
+    os.system("sudo /sbin/ip link set can0 up type can bitrate 400000")
+    print('Press CTL-C to exit')
+    try:
+        bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+    except OSError:
+        print('Cannot find PiCAN board.')
+        exit()
+    
+    
+    runRaspiCodeEvent = Event()
+    runEmergencyEvent = Event()
+    runRaspiCodeEvent.set()
+	
+    #command part
+    threadsense = C.MySensor(bus,runRaspiCodeEvent,runEmergencyEvent)
+    threadcom = C.MyCommand(bus,runRaspiCodeEvent,runEmergencyEvent)
+    
+	#BLE part
+    bleServer = BLEServer()
+    bleTransmitterThread= BLETransmitterThread(bleServer,runRaspiCodeEvent) #for transmitting messages to the server
+    
+    # roadsign detection part
+    thread_roadsign_detector = Thread(target=detection.roadsign_detector, args=(runRaspiCodeEvent,))
+    
+    #seprate the Message
+    seprateMessageThread = sm.SeprateMessageThread(runRaspiCodeEvent)
+    
+    
+    try:
+        threadsense.start()
+        threadcom.start()
+        bleTransmitterThread.start()
+        thread_roadsign_detector.start()
+        seprateMessageThread.start()
+        bleServer.run() ##################################################################################################### a la fin
+				
+    except KeyboardInterrupt:
+        print ('Attempting to close all threads')
+        runRaspiCodeEvent.clear()
+        threadsense.join()
+        threadcom.join()
+        bleTransmitterThread.join()
+        thread_roadsign_detector.join()
+        seprateMessageThread.join()
+    
+        print ('All threads successfully closed')
+        bleServer.quit()
+        """msg = can.Message(arbitration_id=MOT,data=[0x00, 0x00, 0x00,0,0,0,0,0],extended_id=False)
+        bus.send(msg)
+        os.system("sudo /sbin/ip link set can0 down")"""
+		
+if __name__ == '__main__':
+    main()
+
---
+++ demo_sprint3/sharedRessources.py
@@ -0,0 +1,30 @@
+from mutex import mutex
+from threading import Lock
+
+#Global values
+wheelSpeed = 0  
+turbo ="off"
+joystick = "none"
+mode = "assist"
+sign = "none"
+signWidth = 0
+
+
+listMessagesToSend = []
+listMessagesReceived = []
+connectedDevice= False
+	
+
+#Locks
+lockMessagesToSend = Lock()
+lockMessagesReceived = Lock()
+lockConnectedDevice = Lock()
+
+modeLock =Lock()
+joystickLock = Lock()
+turboLock = Lock()
+signLock = Lock()
+signWidthLock = Lock()
+speedLock = Lock()
+
+
