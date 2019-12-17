4c27881f82de57b6a4890159ce4b74fe9b663ee2
frenchfrys <vincezeboss@laposte.net>
2019-12-17 16:17:39 +0100

    sorted training images for search mode. Only kept valid images

--- machineLearning_model/rename_files.py
+++ machineLearning_model/rename_files.py
@@ -3,11 +3,11 @@
 
 import sys, os
 
-input_path = '/home/vincent/Documents/INSA/5A/Projet_SIEC/Road-Analysis-Project/machineLearning_model/training_search/images/augmented_data/output/'
-output_path = "/home/vincent/Documents/INSA/5A/Projet_SIEC/Road-Analysis-Project/machineLearning_model/training_search/images/final_augmented_data/"
+input_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/training_search/images/train"
+output_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/training_search/images/sorted_train/"
 
 
-i = 283
+i = 1
 
 for filename in os.listdir(input_path):
     os.rename(os.path.join(input_path,filename), os.path.join(output_path,str(i)+'.jpg'))
