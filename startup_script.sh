db5a51007723497b1f55c18c0c77efbb74bd63de
TeamRhaegal <teamrhaegalsiec@gmail.com>
2020-01-08 15:15:32 +0100

    corrected raspberry files

--- startup_script.sh
+++ startup_script.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python /home/pi/Documents/projet_SIEC/Road-Analysis-Project/Raspberry/RhaegalRaspi/Demo_sprint4/main.py
+lxterminal -e python /home/pi/Documents/projet_SIEC/Road-Analysis-Project/Raspberry/RhaegalRaspi/Demo_sprint4/main.py
