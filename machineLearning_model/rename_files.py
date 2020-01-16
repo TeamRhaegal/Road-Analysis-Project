757a74b7acce95d45e75c5a39a6569e530df20db
frenchfrys <vincezeboss@laposte.net>
2020-01-16 01:15:06 +0100

    added final training for search mode

--- machineLearning_model/rename_files.py
+++ machineLearning_model/rename_files.py
@@ -2,13 +2,23 @@
 # coding=utf-8
 
 import sys, os
-
-input_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/augmented_new_search_images/renamed_output/"
-output_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/augmented_new_search_images/augmented_data/"
+import glob
 
 
-i = 1
+input_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/training_search/images/train/augmented_data/"
+output_path = "/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/training_search/images/train/temp/"
 
-for filename in os.listdir(input_path):
-    os.rename(os.path.join(input_path,filename), os.path.join(output_path,str(i)+'.jpg'))
-    i = i +1
+image_counter = 1
+xml_counter = 1
+
+FOLDERNAME = input_path
+IMAGE_FOLDER = glob.glob(FOLDERNAME+"*.jpg")
+XML_FOLDER = glob.glob(FOLDERNAME+"*.xml")
+
+for image in IMAGE_FOLDER:
+    os.rename(image, os.path.join(output_path,str(image_counter)+'.jpg'))
+    image_counter = image_counter +1
+
+for csv in XML_FOLDER:
+    os.rename(csv, os.path.join(output_path,str(xml_counter)+'.xml'))
+    xml_counter = xml_counter +1
--- testDataAugmentation/testAugmentator.py
+++ testDataAugmentation/testAugmentator.py
@@ -3,7 +3,7 @@
 
 import Augmentor
 
-p = Augmentor.Pipeline("/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/augmented_new_search_images/") #mettre chemin d'accès vers le dossier des images
+p = Augmentor.Pipeline("/home/vincent/Documents/Travail/INSA_T/5e_annee/ProjetSIEC/github/Road-Analysis-Project/machineLearning_model/new_image_search_renamed/") #mettre chemin d'accès vers le dossier des images
 
 p.skew(probability=0.20)
 #p.random_distortion(probability=0.15)
@@ -14,7 +14,7 @@ p.random_contrast(probability=0.10, min_factor=0.5, max_factor=1.5)
 #p.zoom(probability=0.15,min_factor=50,max_factor=50)
 p.flip_left_right(probability=0.10)
 # Now we can sample from the pipeline:
-p.sample(200)
+p.sample(300)
 
 
 
